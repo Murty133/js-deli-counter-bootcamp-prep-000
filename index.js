5d557632571319ee5fcca01e71ca330f55da6eee
spoonnman <spoonnman@hotmail.com>
2018-10-03 23:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line, person) {
   line.push(person)
   return `Welcome, ${person}. You are number ${line.length}.`
 }
+
+function nowServing(line) {
+  if (line.length === 0){
+    return "There is noby waiting to be served!"
+  }
+  else {
+    return line.pop()
+  }
+}
