d86ba6075dea3fe12a2226a1eb9799edf61f938b
spoonnman <spoonnman@hotmail.com>
2018-10-03 23:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ function nowServing(line) {
   }
 }
 
+/*
+function currentLine(line) {
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+  else {
+    var string = "The line is currently:"
+    for (var i = 0; i < line.length; i++) {
+      string += ` ${i+1}. ${line[i]},`
+    }
+  } return string
+}
+*/
+
 function currentLine(line) {
   if (line.length === 0){
     return "The line is currently empty."
