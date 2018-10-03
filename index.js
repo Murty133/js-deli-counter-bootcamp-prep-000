d2b0acbc7b142e846db5cfb8ae5665fd84ecd66b
spoonnman <spoonnman@hotmail.com>
2018-10-03 23:18:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,10 @@ function currentLine(line) {
   if (line.length === 0){
     return "The line is currently empty."
   }
+  else {
+    var string = "The line is currently: "
+    for (var i = 0; i < line.length; i++) {
+      string += `${i+1}. ${line[i]}`
+    }
+  } return string
 }
